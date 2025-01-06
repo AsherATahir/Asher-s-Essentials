aff74eebdff4cde898e063e0bda4063bc5e3dd87
Asher Tahir <atahir7867@gmail.com>
2025-01-06 18:24:52 -0500

    mobile navbar finished

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,43 +3,35 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <>
-      <nav className="navbar fixed-top navbar-expand-lg navbar-dark navbg">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark navbg justify-content-center">
+        <button
+          className="navbar-toggler position-fixed start-0 ms-3"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <ul className="navbar-nav" style={{ fontFamily: "Oswald, sans-serif" }}>
+          <Link to={"/"} className="navbar-brand m-0 me-md-3">
+            Asher's Essentials
+          </Link>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul
-              className="navbar-nav mx-auto"
-              style={{ fontFamily: "Oswald, sans-serif" }}
-            >
-              <Link
-                to={"/"}
-                className="navbar-brand d-flex justify-content-center m-0 me-md-3"
-              >
-                Asher's Essentials
+            <li className="nav-item">
+              <Link to={"/"} className="nav-link active" aria-current="page">
+                Home
               </Link>
-              <li className="nav-item">
-                <Link to={"/"} className="nav-link active" aria-current="page">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Styles
-                </a>
-              </li>
-            </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Styles
+              </a>
+            </li>
           </div>
-        </div>
+        </ul>
       </nav>
     </>
   );
