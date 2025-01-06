b6eb8ac942450fe2ebe3310d1d0506c18c32795b
Asher Tahir <atahir7867@gmail.com>
2025-01-06 11:30:50 -0500

    UI adjustment to make root 100vw

--- src/components/Selections.tsx
+++ src/components/Selections.tsx
@@ -28,38 +28,40 @@ function Selections({ itemList, loading }: Props) {
 
   return (
     <>
-      <ul
-        className="d-flex flex-row mt-5 justify-content-center overflow-hidden w-100
+      <div className="d-flex justify-content-center">
+        <ul
+          className="d-flex flex-row mt-5 justify-content-center overflow-hidden
     flex-wrap p-0"
-      >
-        {filteredList.map((item, index) => (
-          <li
-            className="d-flex col-lg-4 justify-content-evenly mb-5 col-md-6 col-sm-8"
-            key={index}
-            id="listCard"
-          >
-            <div className="card bg-white shadow-lg">
-              <img
-                id="fetchImage"
-                className="card-img-top"
-                src={item.image}
-                alt="Card image cap"
-              ></img>
-              <div className="card-body">
-                <h5 className="card-title text-subtle lead">
-                  {item.title.length > 25
-                    ? item.title.substring(0, 17) + exceededLength
-                    : item.title}
-                </h5>
-                <p className="card-text"></p>
-                <Link to={`/buy/${item.id}`} className="btn btn-dark shadow">
-                  Buy This!
-                </Link>
+        >
+          {filteredList.map((item, index) => (
+            <li
+              className="d-flex justify-content-evenly mb-5 col-lg-3 col-md-4 col-sm-8 mx-5 col-xl-3"
+              key={index}
+              id="listCard"
+            >
+              <div className="card bg-white shadow-lg">
+                <img
+                  id="fetchImage"
+                  className="card-img-top img-fluid"
+                  src={item.image}
+                  alt="Card image cap"
+                ></img>
+                <div className="card-body">
+                  <h5 className="card-title text-subtle lead">
+                    {item.title.length > 25
+                      ? item.title.substring(0, 17) + exceededLength
+                      : item.title}
+                  </h5>
+                  <p className="card-text"></p>
+                  <Link to={`/buy/${item.id}`} className="btn btn-dark shadow">
+                    Buy This!
+                  </Link>
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
