5f58aa0333fbee2e073b28d2146c3b188c66be43
Asher Tahir <atahir7867@gmail.com>
2025-01-07 14:45:03 -0500

    more mobile navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,34 +15,23 @@ function Navbar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div
-          className="collapse navbar-collapse d-flex justify-content-center"
-          id="navbarSupportedContent"
-        >
-          <ul
-            className="navbar-nav"
-            style={{ fontFamily: "Oswald, sans-serif" }}
-          >
-            <Link to={"/"} className="navbar-brand m-0 me-md-3">
-              Asher's Essentials
-            </Link>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <li className="nav-item">
-                <Link to={"/"} className="nav-link active" aria-current="page">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Styles
-                </a>
-              </li>
-            </div>
-          </ul>
-        </div>
+        <ul className="navbar-nav" style={{ fontFamily: "Oswald, sans-serif" }}>
+          <Link to={"/"} className="navbar-brand m-0 me-md-3">
+            Asher's Essentials
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <li className="nav-item">
+              <Link to={"/"} className="nav-link active" aria-current="page">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Styles
+              </a>
+            </li>
+          </div>
+        </ul>
       </nav>
     </>
   );
