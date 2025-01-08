e164b0125e3da002d983c240e184e87a54a1830a
Asher Tahir <atahir7867@gmail.com>
2025-01-08 16:49:30 -0500

    started on shopping cart feature

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { ShoppingCart } from "lucide-react";
+import { useState } from "react";
 
 function Navbar() {
+  let [cartItems, setItems] = useState<number>(0);
   return (
     <>
       <nav className="navbar fixed-top navbar-expand-lg navbar-dark navbg justify-content-center">
@@ -16,7 +19,7 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <ul className="navbar-nav" style={{ fontFamily: "Oswald, sans-serif" }}>
-          <Link to={"/"} className="navbar-brand m-0 me-md-3">
+          <Link to={"/"} className="navbar-brand m-0 me-lg-3">
             Asher's Essentials
           </Link>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -29,6 +32,16 @@ function Navbar() {
               <a className="nav-link" href="#">
                 Styles
               </a>
+            </li>
+            <li className="nav-item d-flex justify-content-center">
+              <a href="#">
+                <ShoppingCart
+                  size={35}
+                  color="#ffffff"
+                  className="nav-link ms-2 m-lg-0"
+                ></ShoppingCart>
+              </a>
+              <div className="m-0 border rounded itemCircle">{cartItems}</div>
             </li>
           </div>
         </ul>
